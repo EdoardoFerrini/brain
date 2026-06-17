a8aa93eae0bf59f3f7fa34906bad96131273c04e
EdoardoFerrini <EdoardoFerrini@users.noreply.github.com>
2026-06-17 10:21:53 +0000

    deploy: EdoardoFerrini/brain-source@93573a1a92004742505b2aa439775278cd865d3c

--- postscript-674b8e25.js
+++ postscript-674b8e25.js
@@ -5,10 +5,9 @@ await Promise.all([
   import("./static/scripts/script-3-7e107d7e.js"),
   import("./static/scripts/script-4-0e41d61b.js"),
   import("./static/scripts/script-5-5ef560a8.js"),
-  import("./static/scripts/script-6-a770a134.js"),
-  import("./static/scripts/script-7-632b2822.js"),
-  import("./static/scripts/script-8-296b30c3.js"),
-  import("./static/scripts/script-9-5f71ef20.js"),
-  import("./static/scripts/script-10-a2f22c29.js")
+  import("./static/scripts/script-6-632b2822.js"),
+  import("./static/scripts/script-7-296b30c3.js"),
+  import("./static/scripts/script-8-5f71ef20.js"),
+  import("./static/scripts/script-9-a2f22c29.js")
 ]);
-await import("./static/scripts/script-11-7ca62114.js");
+await import("./static/scripts/script-10-7ca62114.js");
